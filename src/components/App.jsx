28152da624401787cfa09c65261fea94952e432a
SemenovIhor <113428082+SemenovIhor@users.noreply.github.com>
2023-01-26 16:26:10 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,11 @@
+import { useTheme } from '@chakra-ui/react';
+import { useMedia } from './Media/useMedia';
+
 export const App = () => {
+  const media = useMedia();
+  console.log(media);
+  const theme = useTheme();
+
   return (
     <div
       style={{
@@ -7,7 +14,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import 'styles/index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from 'styles/theme';
+import Media from 'components/Media/Media';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/wallet-team-project">
+      <ChakraProvider theme={theme}>
+        <Media>
+          <App />
+        </Media>
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
