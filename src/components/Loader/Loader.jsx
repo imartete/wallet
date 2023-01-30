eb61dcacae25bffbb16074baa227b16429781bb5
Turchynyk Oleksii <105796938+TurchynykOleksii@users.noreply.github.com>
2023-01-30 09:02:10 +0200

    fix loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,9 +2,11 @@ import css from './Loader.module.css';
 
 const Loader = () => {
   return (
-    <div className={css.ldsRipple}>
-      <div></div>
-      <div></div>
+    <div className={css.loaderWraper}>
+      <div className={css.ldsRipple}>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
