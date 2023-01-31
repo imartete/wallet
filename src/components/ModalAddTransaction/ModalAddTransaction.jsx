4d7a25401c4121acd0c340c2708226c4c9b16cab
imartete <79273837+imartete@users.noreply.github.com>
2023-01-31 04:37:03 +0100

    style transaction modal

--- src/components/ModalAddTransaction/ModalAddTransaction.jsx
+++ src/components/ModalAddTransaction/ModalAddTransaction.jsx
@@ -4,10 +4,15 @@ import { useDispatch } from 'react-redux';
 import { modalIsOpen } from '../../redux/modal/modalSlice';
 import { AddTransactionForm } from 'components/AddTransactionForm/AddTransactionForm';
 import { ModalWindow, Overlay } from './ModalAddTransaction.styled';
-import { CloseButton } from '@chakra-ui/react';
+import { Box, CloseButton, Text } from '@chakra-ui/react';
+import { useMedia } from 'components/Media/useMedia';
+import CustomIcon from 'components/CustomIcon/CustomIcon';
+import UserMenu from 'components/UserMenu/UserMenu';
 
 const Modal = () => {
   const dispatch = useDispatch();
+  const { isMobile, isNotMobile } = useMedia();
+
   useEffect(() => {
     const onClickEscape = e => {
       if (e.code === 'Escape') {
@@ -29,14 +34,36 @@ const Modal = () => {
   return ReactDOM.createPortal(
     <Overlay onClick={handleBackdrop}>
       <ModalWindow>
-        <CloseButton
-          p="11"
-          pos="absolute"
-          top="5"
-          right="5"
-          size="md"
-          onClick={handleBackdrop}
-        />
+        {isMobile && (
+          <Box
+            mb="20px"
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            w="100%"
+          >
+            <Box display="flex" alignItems="center" gap="15.5px">
+              <CustomIcon name="icon-logo" color="currentColor" size="30px" />
+              <Text fontSize="24px" fontWeight="700">
+                Wallet
+              </Text>
+            </Box>
+            <UserMenu />
+          </Box>
+        )}
+        <Text mb="40px" fontSize={isMobile ? '2xl' : '3xl'}>
+          Add transaction
+        </Text>
+        {isNotMobile && (
+          <CloseButton
+            p="11"
+            pos="absolute"
+            top="5"
+            right="5"
+            size="md"
+            onClick={handleBackdrop}
+          />
+        )}
         <AddTransactionForm onClick={handleBackdrop} />
       </ModalWindow>
     </Overlay>,
